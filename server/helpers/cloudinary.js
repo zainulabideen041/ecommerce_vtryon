da721d73c0747436f4471d110d0a27ca40abb0f8
zainulabideen041 <zainulabideen4578@gmail.com>
2025-03-05 00:29:20 +0500

    environment variables added

--- server/helpers/cloudinary.js
+++ server/helpers/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
 
 cloudinary.config({
-  cloud_name: "dnoi1telk",
-  api_key: "546623679463593",
-  api_secret: "HgFi6uxDPoqQKdkEaozTvmgVsTQ",
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
 });
 
 const storage = new multer.memoryStorage();
--- server/server.js
+++ server/server.js
@@ -56,4 +56,8 @@ app.use("/api/shop/review", shopReviewRouter);
 
 app.use("/api/common/feature", commonFeatureRouter);
 
+app.get("/", (req, res) => {
+  res.json("hello");
+});
+
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
