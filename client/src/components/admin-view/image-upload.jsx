1d0a23a31ef8bc27514390e38c730fdd033d3ef4
zainulabideen041 <zainulabideen4578@gmail.com>
2025-03-06 15:09:44 +0500

    image upload bug fixes

--- client/src/components/admin-view/image-upload.jsx
+++ client/src/components/admin-view/image-upload.jsx
@@ -47,7 +47,7 @@ function ProductImageUpload({
     const data = new FormData();
     data.append("my_file", imageFile);
     const response = await axios.post(
-      "http://localhost:5000/api/admin/products/upload-image",
+      "https://ecomtryonbackend.vercel.app/api/admin/products/upload-image",
       data
     );
 
